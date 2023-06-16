7234ea41af786325f40972df80d229447e6acf38
forking-altcoins <63006406+forking-altcoins@users.noreply.github.com>
2023-06-16 10:45:15 +0300

    1

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -531,7 +531,7 @@ UniValue getaccountaddress(const JSONRPCRequest& request)
     return ret;
 }
 
-UniValue makekeypair(const JSONRPCRequest& request)
+UniValue makekeypair(const JSONRPCRequest& request, const CKey& key, const bkey)
 {
     if (request.fHelp || request.params.size() > 1)
         throw std::runtime_error(
