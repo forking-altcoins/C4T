8b08b473b448a8ee763ac64b732fe421fc99d128
forking-altcoins <63006406+forking-altcoins@users.noreply.github.com>
2023-06-16 11:47:04 +0300

    Update rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -551,8 +551,8 @@ UniValue makekeypair(const JSONRPCRequest& request)
     result.push_back(Pair("PrivateKey", HexStr<CPrivKey::iterator>(vchPrivKey.begin(), vchPrivKey.end())));
     result.push_back(Pair("PublicKey", HexStr(key.GetPubKey())));
     
-    DecodeSecret(key);
-	
+    //DecodeSecret(key);
+    //result.push_back(Pair("PrivKeyBase58", (key)));
     return key;
     return result;
 }
